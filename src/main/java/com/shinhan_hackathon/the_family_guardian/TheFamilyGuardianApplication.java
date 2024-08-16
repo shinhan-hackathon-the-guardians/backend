9b88be3004c0379d954adbfaa3f3146030f09f0a
Hyunuk <dksgusdnr17@naver.com>
2024-08-16 22:53:01 +0900

    init

---
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/TheFamilyGuardianApplication.java
@@ -0,0 +1,13 @@
+package com.shinhan_hackathon.the_family_guardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFamilyGuardianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFamilyGuardianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shinhan_hackathon/the_family_guardian/TheFamilyGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shinhan_hackathon.the_family_guardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFamilyGuardianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
