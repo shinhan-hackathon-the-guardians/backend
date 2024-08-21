d653c79cd55119a982ae1063591100cae699a503
안현욱 <dksgusdnr17@naver.com>
2024-08-21 15:18:12 +0900

    Refactor: Test 호출 방식 수정

--- src/test/java/com/shinhan_hackathon/the_family_guardian/bank/user/BankAccountTest.java
+++ src/test/java/com/shinhan_hackathon/the_family_guardian/bank/user/BankAccountTest.java
@@ -33,84 +33,84 @@ public class BankAccountTest {
     @Test
     @DisplayName("1_상품_등록")
     void createDemandDeposit() {
-        accountService.createDemandDeposit();
+        log.info("{}", accountService.createDemandDeposit());
     }
 
     @Test
     @DisplayName("2_상품_조회")
     void inquireDemandDeposit() {
-        accountService.inquireDemandDepositList();
+        log.info("{}", accountService.inquireDemandDepositList());
     }
 
     @Test
     @DisplayName("3_계좌_생성")
     void createAccount() throws Exception {
-        accountService.createAccount();
+        log.info("{}", accountService.createAccount());
     }
 
     @Test
     @DisplayName("4_계좌_목록_조회")
     void inquireAccountList() throws Exception {
-        accountService.inquireAccountList();
+        log.info("{}", accountService.inquireAccountList());
     }
 
     @Test
     @DisplayName("5_계좌_단건_조회")
     void inquireAccount() {
-        accountService.inquireAccount();
+        log.info("{}", accountService.inquireAccount());
     }
 
     @Test
     @DisplayName("6_예금주_조회")
     void inquireAccountHolderName() {
-        accountService.inquireAccountHolderName();
+        log.info("{}", accountService.inquireAccountHolderName());
     }
 
     @Test
     @DisplayName("7_계좌_잔액_조회")
     void inquireAccountBalance() {
-        accountService.inquireAccountBalance();
+        log.info("{}", accountService.inquireAccountBalance());
     }
 
     @Test
     @DisplayName("8_계좌_츨금")
     void updateAccountWithdrawal() {
-        accountService.updateAccountWithdrawal();
+        log.info("{}", accountService.updateAccountWithdrawal());
     }
 
     @Test
     @DisplayName("9_계좌_입금")
     void updateAccountDeposit() {
-        accountService.updateAccountDeposit();
+        log.info("{}", accountService.updateAccountDeposit());
     }
 
     @Test
     @DisplayName("10_계좌_이체")
     void updateAccountTransfer() {
-        accountService.updateAccountTransfer();
+        log.info("{}", accountService.updateAccountTransfer());
     }
 
     @Test
     @DisplayName("11_계좌_이체_한도_변경")
     void updateTransferLimit() {
-        accountService.updateTransferLimit();
+        log.info("{}", accountService.updateTransferLimit());
     }
 
     @Test
     @DisplayName("12_계좌_거래_내역_조회")
     void inquireAccountTransactionHistoryList() {
-        accountService.inquireTransactionHistoryList();
+        log.info("{}", accountService.inquireTransactionHistoryList());
     }
 
     @Test
     @DisplayName("13_계좌_거래_내역_단건_조회")
     void inquireAccountTransactionHistory() {
-        accountService.inquireTransactionHistory();
+        log.info("{}", accountService.inquireTransactionHistory());
     }
 
     @Test
     @DisplayName("14_계좌_해지")
     void deleteAccount() {
-        accountService.deleteAccount();
+        log.info("{}",accountService.deleteAccount());
     }
 }
