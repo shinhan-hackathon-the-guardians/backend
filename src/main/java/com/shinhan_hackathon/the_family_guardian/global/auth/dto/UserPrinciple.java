dc231fc392a81c269d462354348fdfa669059e0c
namesnames <97gkswn@gmail.com>
2024-08-22 16:07:59 +0900

    [Feat] #21 - gemini chat api 연동

--- src/main/java/com/shinhan_hackathon/the_family_guardian/global/auth/dto/UserPrinciple.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/global/auth/dto/UserPrinciple.java
@@ -8,7 +8,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.Collections;
 
-public record UserPrinciple (
+public record
+UserPrinciple (
         User user
 ) implements UserDetails {
 
