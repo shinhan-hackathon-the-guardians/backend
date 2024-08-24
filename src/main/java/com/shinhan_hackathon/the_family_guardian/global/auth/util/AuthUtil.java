34708a9586d703ec8d58479a657f24ec1e7cfcab
given-dragon <yijy001@gmail.com>
2024-08-25 04:43:13 +0900

    [Add] #20 - UserPrincipal 반환 util추가

--- src/main/java/com/shinhan_hackathon/the_family_guardian/global/auth/util/AuthUtil.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/global/auth/util/AuthUtil.java
@@ -1,6 +1,7 @@
 package com.shinhan_hackathon.the_family_guardian.global.auth.util;
 
 import com.shinhan_hackathon.the_family_guardian.domain.user.entity.User;
+import com.shinhan_hackathon.the_family_guardian.global.auth.dto.UserPrincipal;
 import com.shinhan_hackathon.the_family_guardian.global.auth.service.UserDetailsServiceImpl;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -29,5 +30,13 @@ public class AuthUtil {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         Authentication newAuthentication = createNewAuthentication(authentication, user);
         SecurityContextHolder.getContext().setAuthentication(newAuthentication);
+    }
+
+    public UserPrincipal getUserPrincipal() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication.getPrincipal() == null) {
+            throw new RuntimeException("유저 정보가 세션에 존재하지 않습니다.");
+        }
+        return (UserPrincipal) authentication.getPrincipal();
     }
 }
