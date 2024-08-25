c421ff0c1a432725125b85981a02fca4b6720d75
given-dragon <yijy001@gmail.com>
2024-08-25 23:58:00 +0900

    [Feat] #20 - 가족 구성원 추가 요청 저장 구현

--- src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/repository/ApprovalRepository.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/repository/ApprovalRepository.java
@@ -1,7 +1,12 @@
 package com.shinhan_hackathon.the_family_guardian.domain.approval.repository;
 
 import com.shinhan_hackathon.the_family_guardian.domain.approval.entity.Approval;
+import com.shinhan_hackathon.the_family_guardian.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
+    boolean existsByUser(User user);
+    Optional<Approval> findByUser(User user);
 }
--- src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/service/ApprovalService.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/service/ApprovalService.java
@@ -1,9 +1,15 @@
 package com.shinhan_hackathon.the_family_guardian.domain.approval.service;
 
+import com.shinhan_hackathon.the_family_guardian.domain.approval.entity.AcceptStatus;
+import com.shinhan_hackathon.the_family_guardian.domain.approval.entity.Approval;
 import com.shinhan_hackathon.the_family_guardian.domain.approval.repository.ApprovalRepository;
+import com.shinhan_hackathon.the_family_guardian.domain.family.entity.Family;
+import com.shinhan_hackathon.the_family_guardian.domain.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -12,7 +18,26 @@ public class ApprovalService {
     private final ApprovalRepository approvalRepository;
 
     @Transactional
-    public void createApproval() {
+    public Long createApproval(Family family, User user) {
+        Optional<Approval> optionalApproval = approvalRepository.findByUser(user);
+        if (optionalApproval.isPresent()) {
+            Approval approval = optionalApproval.get();
 
+            switch (approval.getAccepted()) {
+                case PROGRESS -> throw new RuntimeException("초대가 진행중인 유저입니다.");
+                case REFUSE -> throw new RuntimeException("초대를 거절한 유저입니다.");
+                case ACCEPT -> throw new RuntimeException("초대를 수락한 유저입니다.");
+                default -> throw new RuntimeException("초대에 대한 정보가 있으나 응답 상태가 지정되지 않았습니다.");
+            }
+        }
+
+        Approval approval = Approval.builder()
+                .family(family)
+                .user(user)
+                .accepted(AcceptStatus.PROGRESS)
+                .build();
+
+        approval = approvalRepository.save(approval);
+        return approval.getId();
     }
 }
