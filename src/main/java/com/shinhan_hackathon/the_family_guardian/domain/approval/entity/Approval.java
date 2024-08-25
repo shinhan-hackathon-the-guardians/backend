24a2daa2d0a4f3a862e6775147712ea9bbb80178
given-dragon <yijy001@gmail.com>
2024-08-26 01:21:51 +0900

    [Fix] #35 - enumerated 추가

--- src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/entity/Approval.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/entity/Approval.java
@@ -2,14 +2,7 @@ package com.shinhan_hackathon.the_family_guardian.domain.approval.entity;
 
 import com.shinhan_hackathon.the_family_guardian.domain.family.entity.Family;
 import com.shinhan_hackathon.the_family_guardian.domain.user.entity.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -36,6 +29,7 @@ public class Approval { // 패밀리 가입 요청
     private User user;
 
     @Column(nullable = false, columnDefinition = "varchar(10) default 'PROGRESS'")
+    @Enumerated(EnumType.STRING)
     private AcceptStatus accepted;
 
     @Builder
