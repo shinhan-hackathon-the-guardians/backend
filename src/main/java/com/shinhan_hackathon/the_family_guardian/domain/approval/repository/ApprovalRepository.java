aa1e276d9acdfffaa26371d8c2a0b113fc5565da
given-dragon <yijy001@gmail.com>
2024-08-29 17:16:13 +0900

    [Feat] #75 - 대기중인 가족 초대 조회 구현

--- src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/repository/ApprovalRepository.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/domain/approval/repository/ApprovalRepository.java
@@ -4,9 +4,12 @@ import com.shinhan_hackathon.the_family_guardian.domain.approval.entity.Approval
 import com.shinhan_hackathon.the_family_guardian.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ApprovalRepository extends JpaRepository<Approval, Long> {
     boolean existsByUser(User user);
     Optional<Approval> findByUser(User user);
+
+    List<Approval> findAllByUser(User user);
 }
