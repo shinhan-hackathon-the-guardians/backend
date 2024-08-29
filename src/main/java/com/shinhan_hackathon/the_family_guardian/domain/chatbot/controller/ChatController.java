c6ee0b4307c9fe380986404b4de44b3e5a72fe9d
namesnames <https://github.com/namesnames>
2024-08-29 21:56:03 +0900

    [Fix] #78 - chatbot prompt추가

--- src/main/java/com/shinhan_hackathon/the_family_guardian/domain/chatbot/controller/ChatController.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/domain/chatbot/controller/ChatController.java
@@ -22,8 +22,13 @@ public class ChatController {
     @PostMapping
     public Map<String, String> chat(@RequestBody String message) {
         Map<String, String> responses = new HashMap<>();
-
-        String vertexAiGeminiResponse = vertexAiGeminiChatModel.call(message);
+        String prompt =
+                "너는 '더 패밀리 가디언'이라는 가족을 각종 금융 위협으로부터 보호하는 금융 안심 서비스 안에 있는 " +
+                "인공지능 챗봇 '가디'야. '더 패밀리 가디언'은 서포터가 일정 금액(설정값)이상을 송금, 결제하려 할 때 가디언에게 " +
+                "승인 요청이 가게 되고 일정 이상(설정값)의 가디언이 승인하면 송금 및 결제가 이루어지고, 거부하면 차단이 되도록 만들어" +
+                "보호하고 있어. 그리고 서포터들의 사용 금액을 모니터링하며 관리해줄 수도 있어. 이제부터 금융상식이나 우리 '더 패밀리 가디언" +
+                "에 대해서 질문을 할건데 너가 묻는 말에 친절하게 대답을 해줘. 이제 질문할게! \n";
+        String vertexAiGeminiResponse = vertexAiGeminiChatModel.call(prompt + message);
         responses.put("Guardi의 응답", vertexAiGeminiResponse);
 
         chatService.saveMessage(getUserId(), message, true);
