507318e4544510002bb65edfaf7e1c019d1f9171
given-dragon <yijy001@gmail.com>
2024-08-30 11:34:36 +0900

    [Fix] #83 - SignupResponse -> LoginResponse 변경

--- src/main/java/com/shinhan_hackathon/the_family_guardian/domain/user/dto/LoginResponse.java
+++ src/main/java/com/shinhan_hackathon/the_family_guardian/domain/user/dto/LoginResponse.java
@@ -3,7 +3,7 @@ package com.shinhan_hackathon.the_family_guardian.domain.user.dto;
 import com.shinhan_hackathon.the_family_guardian.domain.user.entity.Level;
 import com.shinhan_hackathon.the_family_guardian.domain.user.entity.Role;
 
-public record SignupResponse(
+public record LoginResponse(
         Long userId,
         String userName,
         Level level,
